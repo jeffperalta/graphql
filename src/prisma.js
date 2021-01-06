76d431fce13308e682ee356217a5a0ddb4f0a093
Jeff Peralta <jeff@tsscore.com>
2021-01-06 17:57:58 +0800

    Setup Fragments

--- src/prisma.js
+++ src/prisma.js
@@ -1,9 +1,11 @@
 import { Prisma } from 'prisma-binding'
+import { fragmentReplacements } from './resolvers/index'
 
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: 'http://localhost:4466',
-    secret: 'dGQtKlE7Ob0rK7ObxHw4'
+    secret: 'dGQtKlE7Ob0rK7ObxHw4',
+    fragmentReplacements
 })
 
 export { prisma as default } 
--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,15 +1,17 @@
 import getUserId from '../utils/getUserId'
 
 const User = {
-    email(parent, args, { request }, info) {
-        const userId = getUserId(request, false)
+    email: {
+        fragment: 'fragment userId on User { id }',
+        resolve(parent, args, { request }, info) {
+            const userId = getUserId(request, false)
 
-        if (userId && parent.id === userId) {
-            return parent.email
-        }else{
-            return null
+            if (userId && parent.id === userId) {
+                return parent.email
+            }else{
+                return null
+            }
         }
-
     }
     // posts(parent, args, {db}, info) {
     //     return db.posts.filter(p => p.author === parent.id);

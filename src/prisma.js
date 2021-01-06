31f1910e4d71d737192eef88a0559691ddbcd75e
Jeff Peralta <jeff@tsscore.com>
2021-01-06 10:37:57 +0800

    Close direct access to prisma

--- src/prisma.js
+++ src/prisma.js
@@ -2,7 +2,8 @@ import { Prisma } from 'prisma-binding'
 
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
-    endpoint: 'http://localhost:4466'
+    endpoint: 'http://localhost:4466',
+    secret: 'dGQtKlE7Ob0rK7ObxHw4'
 })
 
 export { prisma as default } 

2667fd315762aa243fca309d3b060066edeacba5
Jeff Peralta <jeff@tsscore.com>
2021-01-07 18:34:37 +0800

    Production environment

--- src/prisma.js
+++ src/prisma.js
@@ -4,7 +4,7 @@ import { fragmentReplacements } from './resolvers/index'
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: process.env.PRISMA_ENDPOINT,
-    secret: 'dGQtKlE7Ob0rK7ObxHw4',
+    secret: process.env.PRISMA_SECRET,
     fragmentReplacements
 })
 
